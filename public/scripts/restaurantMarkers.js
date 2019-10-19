07973a28c61159c5111d3c8d492c1176a56f1338
Jenny Nguy <j.nguy809@gmail.com>
2019-10-19 15:14:41 +0000

    add dummy markers

---
+++ public/scripts/restaurantMarkers.js
@@ -0,0 +1,37 @@
+eqfeed_callback(
+  {
+    type: "Feature",
+    features: [
+      {
+        type: "Feature",
+        properties: {
+          place: "Lighthouse Labs",
+          url: "https://www.lighthouselabs.com",
+          status: "REVIEWED"
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [
+            43.644190, -79.402022
+          ]
+        },
+        id: "1"
+      },
+      {
+        type: "Feature",
+        properties: {
+          place: "Citizen",
+          url: "https://www.elcitizano.com",
+          status: "REVIEWED"
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [
+            43.645316, -79.397570
+          ]
+        },
+        id: "2"
+      }
+    ]
+  },
+)
