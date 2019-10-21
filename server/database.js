3a92bef6df1e9720527c43b517654673484bffac
Jenny Nguy <j.nguy809@gmail.com>
2019-10-21 21:13:49 +0000

    generate menu from db

--- server/database.js
+++ server/database.js
@@ -48,3 +48,11 @@ const addUser =  function(user) {
     .catch(err => console.error('query error', err.stack));
 }
 exports.addUser = addUser;
+
+const getMenu = function(id) {
+  return db.query(`SELECT items.name as item_name, restaurants.name as restaurant_name FROM items
+  JOIN menus ON menus.id = menu_id
+  JOIN restaurants ON restaurant_id = restaurants.id
+  WHERE restaurant_id = $1;`, [id])
+}
+exports.getMenu = getMenu;
