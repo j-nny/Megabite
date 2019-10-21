b573992757ed9b86165cc41dd2ad31a241f78107
peterhangg <peterhanggg@gmail.com>
2019-10-21 16:37:25 +0000

    edit login route and pages

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
-//REFERENCE ONLY:
-
-=======
->>>>>>> routes
 // $(() => {
 //   $.ajax({
 //     method: "GET",
 //     url: "/api/users"
-<<<<<<< HEAD
-//   }).done((data) => {
-//     for(user of data.users) {
-=======
 //   }).done((users) => {
 //     for(user of users) {
->>>>>>> routes
 //       $("<div>").text(user.name).appendTo($("body"));
 //     }
 //   });;
--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,2 +1,2 @@
-INSERT INTO users (name, email, password, number)
-    VALUES ('Devin Sanders', 'tristanjacobs@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '6471111111');
+INSERT INTO users (name, number, email, password)
+    VALUES ('Devin Sanders', '6471111111', 'tristanjacobs@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
