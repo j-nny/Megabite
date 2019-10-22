1f5db2bcef8d5820b5e0207e705aca5e2c55434c
Jenny Nguy <j.nguy809@gmail.com>
2019-10-22 20:52:40 +0000

    add remove button functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -17,36 +17,55 @@
 $(document).ready(function() {
   let allOrders = {};
   $(".item").click( function(evt) {
-    let id = evt.target.parentNode.id;
+    // let id = evt.target.parentNode.id;
+    let id = $(this).data('id');
     console.log("clicked on id: ", id);
     // The item was already clicked on before
     if (allOrders[id]) {
       allOrders[id].quantity += 1;
       $("#cart-item").empty();
       for (let id in allOrders) {
-        $newInput = $(`<input id="item_${id}" class="quantity" type="number" value="${allOrders[id].quantity}" min="0"> ${allOrders[id].name} | Price: ${(allOrders[id].price * allOrders[id].quantity).toFixed(2)}</br>`);
+        $newInput = $(`<input id="item_${id}" class="quantity" type="number" value="${allOrders[id].quantity}" min="1"> ${allOrders[id].name} | Price: ${(Number(allOrders[id].price) * allOrders[id].quantity).toFixed(2)}</br>`);
         $("#cart-item").append($newInput);
+        $newBtn = $(`<button type="button" class="remove-btn">Remove</button></br>`)
+        $("#cart-item").append($newBtn);
+        $($newBtn).on("click", function(evt) {
+          // evt.preventDefault();
+          console.log(">>>>", evt.target)
+        })
+        $($newInput).on("change", (evt) => {
+          // console.log($(this).val());
+          console.log(evt);
+          console.log(allOrders);
+          allOrders[id].quantity = parseInt(evt.target.value)
+        })
       }
 
     } else {
-      $("#cart-item").empty();
       // The item has never been clicked on yet
+      $("#cart-item").empty();
       allOrders[id] = { quantity: 1, price: $(this).children('span').text(), name: $(this).children('#item-name').text() }
       console.log(allOrders)
       for (let id in allOrders) {
-        $newInput = $(`<input id="item_${id}" class="quantity" type="number" value="${allOrders[id].quantity}" min="0"> ${allOrders[id].name} | Price: ${(allOrders[id].price * allOrders[id].quantity).toFixed(2)}</br>`);
+        $newInput = $(`<div class="cart${id}"><input id="${id}" class="quantity" type="number" value="${allOrders[id].quantity}" min="1"> ${allOrders[id].name} | Price: ${(Number(allOrders[id].price) * allOrders[id].quantity).toFixed(2)}</br>`);
         $("#cart-item").append($newInput);
+        $newBtn = $(`<div><button type="button" class="cart${id} remove-btn">Remove</button></br></div>`)
+        $("#cart-item").append($newBtn);
+        $($newBtn).on("click", function(evt) {
+          delete allOrders[id];
+          $(`.cart${id}`).empty();
+        })
+
+        $($newInput).bind("mouseup", (evt) => {
+          console.log(evt);
+          console.log(allOrders);
+          allOrders[id].quantity = parseInt(evt.target.value)
+        })
       }
-      // $newInput.find('input').on("change", () => {
-      //   alert('found it');
-      // })
     }
+
     console.log(allOrders);
   });
 
-  $("#quantity").keyup(function() {
-    if ($("#quantity").html() <= 0) {
 
-    }
-  })
 })
