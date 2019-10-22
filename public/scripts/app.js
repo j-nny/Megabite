a0a1b3a560a1ea68e2b28e49c80455e2eaf57c79
Jenny Nguy <j.nguy809@gmail.com>
2019-10-22 17:31:44 +0000

    add cart functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,22 +11,38 @@
 //   });;
 // });
 
+
+
 //cart quantity goes to 0, removes from cart
 $(document).ready(function() {
-  let allOrders = [];
-  let currentOrder = {id: null,
-    menu
-  }
-  $(".item").click( function() {
-    if (currentOrder.id) {
-      currentOrder.quantity = (currentOrder.quantity + 1);
+  let allOrders = {};
+  $(".item").click( function(evt) {
+    let id = evt.target.parentNode.id;
+    console.log("clicked on id: ", id);
+    // The item was already clicked on before
+    if (allOrders[id]) {
+      allOrders[id].quantity += 1;
+      $("#cart-item").empty();
+      for (let id in allOrders) {
+        $newInput = $(`<input id="item_${id}" class="quantity" type="number" value="${allOrders[id].quantity}" min="0"> ${allOrders[id].name} | Price: ${(allOrders[id].price * allOrders[id].quantity).toFixed(2)}</br>`);
+        $("#cart-item").append($newInput);
+      }
+
     } else {
-      currentOrder.id = "findID"
-      currentOrder.quantity = 1;
-      $("#cart-item").append(`<input class="quantity" type="number" value="${currentOrder.quantity}" min="0"> Cha Siu Bao | Price:</br>`);
+      $("#cart-item").empty();
+      // The item has never been clicked on yet
+      allOrders[id] = { quantity: 1, price: $(this).children('span').text(), name: $(this).children('#item-name').text() }
+      console.log(allOrders)
+      for (let id in allOrders) {
+        $newInput = $(`<input id="item_${id}" class="quantity" type="number" value="${allOrders[id].quantity}" min="0"> ${allOrders[id].name} | Price: ${(allOrders[id].price * allOrders[id].quantity).toFixed(2)}</br>`);
+        $("#cart-item").append($newInput);
+      }
+      // $newInput.find('input').on("change", () => {
+      //   alert('found it');
+      // })
     }
-    console.log(currentOrder);
-  })
+    console.log(allOrders);
+  });
 
   $("#quantity").keyup(function() {
     if ($("#quantity").html() <= 0) {
