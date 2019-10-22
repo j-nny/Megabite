5654ca1228edbc99e4201cbfb8c4a39c20afc241
Jenny Nguy <j.nguy809@gmail.com>
2019-10-22 21:42:54 +0000

    tweak price updates

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
         })
 
         $($newInput).bind("input", (evt) => {
-          evt.target.parentNode.children[1].innerHTML = evt.target.value * allOrders[id].price;
+          evt.target.parentNode.children[1].innerHTML = (evt.target.value * allOrders[id].price).toFixed(2);
         })
       }
 
@@ -45,7 +45,7 @@ $(document).ready(function() {
       allOrders[id] = { quantity: 1, price: $(this).children('span').text(), name: $(this).children('#item-name').text() }
       console.log(allOrders)
       for (let id in allOrders) {
-        $newInput = $(`<div class="cart${id}"><input id="${id}" class="quantity" type="number" value="${allOrders[id].quantity}" min="1"> ${allOrders[id].name} | Price: ${(Number(allOrders[id].price) * allOrders[id].quantity).toFixed(2)}</br>`);
+        $newInput = $(`<div class="cart${id}"><input id="${id}" class="quantity" type="number" value="${allOrders[id].quantity}" min="1"> ${allOrders[id].name} | Price: <span class="cart-item-price${id}">${(Number(allOrders[id].price) * allOrders[id].quantity).toFixed(2)}</span></br>`);
         $("#cart-item").append($newInput);
         $newBtn = $(`<div><button type="button" class="cart${id} remove-btn">Remove</button></br></div>`)
         $("#cart-item").append($newBtn);
@@ -54,10 +54,8 @@ $(document).ready(function() {
           $(`.cart${id}`).empty();
         })
 
-        $($newInput).bind("mouseup", (evt) => {
-          console.log(evt);
-          console.log(allOrders);
-          allOrders[id].quantity = parseInt(evt.target.value)
+        $($newInput).bind("input", (evt) => {
+          evt.target.parentNode.children[1].innerHTML = (evt.target.value * allOrders[id].price).toFixed(2);
         })
       }
     }
