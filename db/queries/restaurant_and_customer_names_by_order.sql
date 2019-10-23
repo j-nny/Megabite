e96b2041c160f10e73695e4620f717e43b92bd36
jad <jad_dis@hotmail.com>
2019-10-23 18:59:36 +0000

    added order submission and according database insertions

--- db/queries/restaurant_and_customer_names_by_order.sql
+++ db/queries/restaurant_and_customer_names_by_order.sql
@@ -1,7 +1,7 @@
-SELECT orders.id, orders.quantity, orders.time_entered, orders.time_promised, CONCAT(users.first_name, ' ',  users.last_name) as customer_name, restaurants.name as restaurant_name
+SELECT orders.id, orders.time_entered, orders.time_promised, CONCAT(users.first_name, ' ',  users.last_name) as customer_name, restaurants.name as restaurant_name
 FROM orders
 JOIN users ON orders.customer_id = users.id
 JOIN restaurants ON restaurants.id = orders.restaurant_id
-WHERE users.first_name = 'Link'
+WHERE users.first_name = 'Rick'
 GROUP BY orders.id, users.first_name, users.last_name, restaurants.name
 ORDER BY time_promised;
