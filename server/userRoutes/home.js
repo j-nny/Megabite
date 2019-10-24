730556a002d3472682e5bbf2daa430aa45ee0447
Jenny Nguy <j.nguy809@gmail.com>
2019-10-24 16:30:23 +0000

    resolve merge

--- server/userRoutes/home.js
+++ server/userRoutes/home.js
@@ -4,17 +4,10 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   if (!req.session.user_id) {
-<<<<<<< HEAD
-    let templateVar = { user: req.session.user_id };
-    res.render("login", templateVar);
-  } else {
-    res.redirect("/browse");
-=======
   let templateVar = { user: req.session.user_id };
   res.render("login", templateVar);
 } else {
   res.redirect("/browse");
->>>>>>> origin/master
   }
 });
 
