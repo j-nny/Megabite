0c955fbae150efcf5f7faeec184890eb9f122f03
Jenny Nguy <j.nguy809@gmail.com>
2019-10-24 04:10:45 +0000

    finish UI for order history

--- server/ownerRoutes/ownerOrder.js
+++ server/ownerRoutes/ownerOrder.js
@@ -4,7 +4,11 @@ const db = require("../database");
 
 router.get('/', (req, res) => {
   res.render("ownerOrder");
+});
 
+router.post('/', (req, res) => {
+  res.render("ownerOrder");
+  // query to make order active false
 });
 
 module.exports = router;
